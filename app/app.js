9f48f53ad87542afc3f0386b4f2175bbb3d0e0c4
Christian <christianfscott@gmail.com>
2016-08-30 01:17:37 +1200

    yea i forgot already

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 /*
-  TODO:
-    - implement nicknames
-    - add dynamic user count to page
-    - fix <li> css to allow larger messages
-    - make responsive
-    - make a chat bot (lol)
-    - delete messages across all instances (give msgs unique id's???)
+Backend for simple socket.io chat app
+TODO:
+  - implement nicknames
+  - add dynamic user count to page
+  - fix <li> css to allow larger messages
+  - make responsive
+  - make a chat bot (lol)
+  - delete messages across all instances (give msgs unique id's???)
 */
 
 // set up basic express server
@@ -17,59 +18,54 @@ const validator = require('validator');
 
 const PORT = 3100;
 
-// routing
+// serve files from /dist, relative to current directory
 app.use(express.static(__dirname + '/dist'))
 
 app.get('/', (req, res) => {
+  // all functionality exists in index
   res.sendfile('index.html')
 })
 
 // chatroom
-let numUsers = 0;
-let currentUsers = {};
+let currentUsers = [];
 
 io.on('connection', (socket) => {
 
-  numUsers++;
-  console.log('users: ' + numUsers);
-
+  // when a user connects, send a
+  // list of the current users
   socket.emit('setup', {
-    currentUsers: currentUsers
+    currentUsers: currentUsers,
+    numUsers: currentUsers.length
   })
 
   socket.on('new user', (data) => {
+    // when the server receives a 'new user' event
+    // from the client, that user's userName is
+    // escaped and their userName and connect time
+    // are sent to every client, along with an updated
+    // user count
     let cleanName = validator.escape(data.username);
     let user = {
       username: cleanName,
       connectTime: data.connectTime,
-      numUsers: numUsers,
+      numUsers: currentUsers.length
     }
 
-    currentUsers[cleanName] = {
-      username: cleanName,
-      connectTime: data.connectTime,
-      numUsers: numUsers,
-    }
-
-    console.log(cleanName + ' connected');
     io.emit('new user', user);
   })
 
   socket.on('chat message', (data) => {
     let cleanMsg = validator.escape(data.msg);
     let cleanName = validator.escape(data.username)
-    console.log(
-      cleanName + ' says: ' + cleanMsg
-    );
+
     io.emit('chat message', {
       msg: cleanMsg,
       username: cleanName
     })
   });
 
-  socket.on('disconnect', () => {
-    numUsers--;
-    console.log('users: ' + numUsers);
+  socket.on('disconnect', (data) => {
+    io.emit('user disconnect', data);
   })
 
 })
--- app/dist/main.js
+++ app/dist/main.js
@@ -1,30 +1,53 @@
-const socket = io()
+/* Client side code for simple socket.io chat app */
 
-let dt = new Date()
+// init variables
+const socket = io();
+const dt = new Date();
+
+const $textBox = $('#text-box'); //
+const $messages = $('#messages');
+const $noMessages = $('#no-messages');
+const $numUsers = $('#num-users');
+const $whoOnline = $('#who-online');
+
 let msgCount = 0;
 let username;
 
+
+// init & parse templates for messages and
+// active users defined in index.html
 let msgTemplate = $('#msg-template').html();
 let userTemplate = $('#user-template').html();
 Mustache.parse(msgTemplate);
 Mustache.parse(userTemplate);
 
+// helper functions
+
 function getTime() {
-  return dt.getHours() + ":" + dt.getMinutes();
+  return dt.getHours() + ":" + (dt.getMinutes()<10?'0':'') + dt.getMinutes();
 }
 
 function mention(data) {
+  // when a username is clicked, focus chat input and
+  // change value to @username
+  // TODO: change to jquery and move to global scope
   const messageBox = document.getElementById('text-box');
   messageBox.value = `@${ data } `;
   messageBox.focus();
 }
 
-function deleteMessage(data) {
+function deleteUser(data) {
+  // remove parent of '✕' from the DOM,
+  // which is the enclosing message element
   const usr = $(data).parent();
   usr.remove();
 }
 
-function deleteUser(data) {
+function deleteMessage(data) {
+  // remove parent of '✕' from the DOM,
+  // parent being the enclosing message element.
+  // then, if the message count is zero, the
+  // #no-messages li is made visible
   const msg = $(data).parent();
   msg.remove();
   msgCount--;
@@ -33,16 +56,39 @@ function deleteUser(data) {
   }
 }
 
+class NotImplementedError extends Error {
+
+  constructor(message) {
+    super(message);
+    this.name = 'NotImplementedError';
+    this.message = message || 'feature not yet implemented';
+
+  }
+}
+
 $('form').submit(() => {
-  socket.emit('chat message', {
-    msg: $('#text-box').val(),
-    username,
-  });
-  $('#text-box').val('');
+    // emit a new message event if the input
+    // is not empty
+    if ($textBox.val().length !== 0) {
+      socket.emit('chat message', {
+        msg: $textBox.val(),
+        username,
+      });
+      $textBox.val('');
+    }
   return false
 })
 
+// socket.io stuff
+socket.on('user disconnect', (data) => {
+  throw new NotImplementedError('functionality to handle user ' +
+                                'disconnect has ' +
+                                'not been implemented yet.')
+})
+
 socket.on('chat message', (data) => {
+  // message count is incremented by one then
+  // the message card is rendered and added to the DOM
   msgCount++;
   let newMsg = Mustache.render(msgTemplate, {
     username: data.username,
@@ -50,26 +96,30 @@ socket.on('chat message', (data) => {
     msg: data.msg,
   });
 
-  $('#messages').append(newMsg);
-  $('#no-messages').hide('fast');
+  $messages.append(newMsg);
+  $noMessages.hide('fast');
 })
 
 socket.on('new user', (data) => {
-  console.log(data);
-  $('#num-users').html(data.numUsers);
+  // when a new user connects, the user count
+  // is updated and a new user card is added
+  // to the side bar
+  $numUsers.html(data.numUsers);
+  console.log(data.numUsers);
   let newUser = Mustache.render(userTemplate, {
     username: data.username,
     connectTime: data.connectTime,
   })
-  $('#who-online').append(newUser);
+  $whoOnline.append(newUser);
 })
 
 $(() => {
+  // assign user a random username on load
+  // and emit new user event
   username = "user" + Math.floor(Math.random()*10000)
   socket.emit('new user', {
     username: username,
     connectTime: getTime(),
     numUsers: 0
   });
-  return false;
 })
